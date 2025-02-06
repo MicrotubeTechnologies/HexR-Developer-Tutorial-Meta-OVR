c00921cfe1319431167a0738ea7c2b85f4b3fa5f
echang007 <edmond@microtube.tech>
2025-02-06 21:09:16 +0800

    Updates

--- Assets/HexRAssets/Main Script/HaptGloveManager.cs
+++ Assets/HexRAssets/Main Script/HaptGloveManager.cs
@@ -86,7 +86,7 @@ namespace HexR
                 BatteryState(LeftOrRight);
 
                 // Wait for 8 seconds before continuing the loop
-                yield return new WaitForSeconds(14f);
+                yield return new WaitForSeconds(8f);
             }
         }
         private void BatteryState(String LeftOrRight)
@@ -94,12 +94,27 @@ namespace HexR
             if(LeftOrRight == "Right")
             {
                 float BatteryLevel = rightHand.GetBatteryLevel();
-                RightBtText.text = "Right Glove Connected: " + BatteryLevel*100 + "%";
+                if(BatteryLevel == 0)
+                {
+                    RightBtText.text = "Right Glove Ready";
+                }
+                else
+                {
+                    RightBtText.text = "Right Glove Ready: " + Math.Round(BatteryLevel * 100) + "%";
+                }
             }
             else if(LeftOrRight == "Left")
             {
                 float BatteryLevel = leftHand.GetBatteryLevel();
-                LeftBtText.text = "Left Glove Connected: " + BatteryLevel*100 + "%";
+                if (BatteryLevel == 0)
+                {
+                    LeftBtText.text = "Left Glove Ready";
+                }
+                else
+                {
+                    LeftBtText.text = "Left Glove Ready: " + Math.Round(BatteryLevel * 100) + "%";
+                }
+
             }
 
         }
